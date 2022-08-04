e64ff03f154139e0d72d90d87356368e1278300f
Lesly Alvarez <leslyalvarez08@gmail.com>
2022-08-04 15:04:27 -0500

    each letter button displays corresponding letter

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 let words = ["education", "voter", "principle", "defendant", "elapse", "incentive"];
 
 
+let letterBtn = document.querySelectorAll(".letterBtn")
 let displayedWord = document.querySelector("p");
 let newWord = document.querySelector(".newWord");
 
 newWord.addEventListener('click', () =>{
    displayedWord.textContent = words[Math.floor(Math.random() * words.length)];
 });
+
+letterBtn.forEach((letter) => {
+    letter.addEventListener('click', () =>{
+    displayedWord.textContent = letter.textContent;
+})
+ });
+
