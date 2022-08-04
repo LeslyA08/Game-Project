aa396f958fa63367b1344f3e1e2cce108036986d
Lesly Alvarez <leslyalvarez08@gmail.com>
2022-08-04 12:35:19 -0500

    Random words appear each time the button is clicked, not just once.

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-const words = ["education", "voter", "principle", "defendant", "elapse", "incentive"];
-const chosenWord = words[Math.floor(Math.random()*words.length)];
-const chosenWord2 = words[Math.floor(Math.random()*words.length)];
+let words = ["education", "voter", "principle", "defendant", "elapse", "incentive"];
+
 
 let displayedWord = document.querySelector("p");
-const newWord = document.querySelector(".newWord");
+let newWord = document.querySelector(".newWord");
 
 newWord.addEventListener('click', () =>{
-    if (displayedWord === ""){
-    displayedWord.textContent = chosenWord;
-    } else {
-    displayedWord.textContent = chosenWord2;
-    }
+   displayedWord.textContent = words[Math.floor(Math.random() * words.length)];
 });
