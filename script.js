0a0d3ef63e0820602415442436a928c175074daa
Lesly Alvarez <leslyalvarez08@gmail.com>
2022-08-08 15:07:46 -0500

    See Word button now reveals the current word

--- script.js
+++ script.js
@@ -5,37 +5,32 @@ let letterBtn = document.querySelectorAll(".letterBtn")
 let displayedWord = document.querySelector(".wordDisplay");
 let newWord = document.querySelector(".newWord");
 let randomWord =words[Math.floor(Math.random() * words.length)]
-let finalWord = words[Math.floor(Math.random() * words.length)].replace(/[a-z]/gi, '-');
+let finalWord = randomWord.replace(/[a-z]/gi, '-');
 let letter = "A-Z"
-let lettersDiv = document.querySelector(".lettersDiv")
-const wordDisplay = document.querySelector(".wordDisplay")
+let lettersDiv = document.querySelector(".lettersDiv");
+let revealWord = document.querySelector(".revealWord")
 
 newWord.addEventListener('click', () =>{
-    displayedWord.textContent = words[Math.floor(Math.random() * words.length)].replace(/[a-z]/gi, '-');
+    displayedWord.textContent = finalWord;
  });
 
-
+revealWord.addEventListener('click', () =>{
+    displayedWord.textContent = randomWord;
+})
 
 letterBtn.forEach((letter) => {
     letter.addEventListener('click', () =>{
-        // console.log("click");
         let charArray = randomWord.split("");
-        let dashes = finalWord
-        // console.log("what is charArray: " + charArray)
+        let dashes = finalWord;
         if (charArray.includes((letter.textContent).toLowerCase())) {
-            // console.log("entered loop")
             charArray.forEach((char, index) => {
                 if (char == letter.textContent.toLowerCase()){
-                   // console.log("checking letters: " + char + " index: " + index)
                     const dashArr = dashes.split("");
                     dashArr[index] = char;
                     let newString = dashArr.join("");
-                    wordDisplay.textContent = newString;
+                    displayedWord.textContent = newString;
                 }
             })
         }
     })
 })
-
- //console.log(randomWord)
-
