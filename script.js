11403f2a6f2f1984701012062ae53a3c5c67847f
Lesly Alvarez <leslyalvarez08@gmail.com>
2022-08-08 08:46:08 -0500

    got a single letter to replace a dash

--- script.js
+++ script.js
@@ -5,17 +5,37 @@ let letterBtn = document.querySelectorAll(".letterBtn")
 let displayedWord = document.querySelector(".wordDisplay");
 let newWord = document.querySelector(".newWord");
 let randomWord =words[Math.floor(Math.random() * words.length)]
-let dashedWord = randomWord.toString();
-
+let finalWord = words[Math.floor(Math.random() * words.length)].replace(/[a-z]/gi, '-');
+let letter = "A-Z"
+let lettersDiv = document.querySelector(".lettersDiv")
+const wordDisplay = document.querySelector(".wordDisplay")
 
 newWord.addEventListener('click', () =>{
     displayedWord.textContent = words[Math.floor(Math.random() * words.length)].replace(/[a-z]/gi, '-');
  });
 
- letterBtn.forEach((letter) => {
-    letter.addEventListener('click', () =>{
-    displayedWord.textContent = letter.textContent;
-})
- });
 
+
+letterBtn.forEach((letter) => {
+    letter.addEventListener('click', () =>{
+        // console.log("click");
+        let charArray = randomWord.split("");
+        let dashes = finalWord
+        // console.log("what is charArray: " + charArray)
+        if (charArray.includes((letter.textContent).toLowerCase())) {
+            // console.log("entered loop")
+            charArray.forEach((char, index) => {
+                if (char == letter.textContent.toLowerCase()){
+                   // console.log("checking letters: " + char + " index: " + index)
+                    const dashArr = dashes.split("");
+                    dashArr[index] = char;
+                    let newString = dashArr.join("");
+                    wordDisplay.textContent = newString;
+                }
+            })
+        }
+    })
+})
+
+ //console.log(randomWord)
 
